05967808ce1c7e91b0a0d618c03ef1a386e23543
Rosa-Palaci <palacibelrose431@gmail.com>
2024-04-25 12:41:16 -0600

    Version R

--- js/marvel.js
+++ js/marvel.js
@@ -1,15 +1,7 @@
-const apibutton = document.getElementById("apibutton");
-const apiData = document.getElementById("apiData");
+let input = document.getElementById("input-box");
+let button = document.getElementById("submit-button");
+let showContainer = document.getElementById ("show-container");
+let listContainer =document.getElementById(".list");
 
-const callAPI = () => {
-  fetch(
-    "https://gateway.marvel.com/v1/public/characters?ts=12345678&apikey=393649b76ca1c0fe1aacb077b44d6351&hash=6f5eb1c9303fa4f03102014254a82704"
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      apiData.innerText = JSON.stringify(data.name);
-    })
-    .catch((e) => console.log(new Error(e)));
-};
-
-apibutton.addEventListener("click", callAPI);
+let date = new Date();
+console.log(date.getTime());
