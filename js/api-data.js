3d555813f0e54a13fc4106cac83df4ef3dfdc53f
Rosa-Palaci <palacibelrose431@gmail.com>
2024-04-25 18:40:01 -0600

    conexion al api caracters

--- js/api-data.js
+++ js/api-data.js
@@ -1,3 +1,3 @@
-let ts = "12345678";
-let publickey = "393649b76ca1c0fe1aacb077b44d6351";
-let hashval = "6f5eb1c9303fa4f03102014254a82704";
+let ts = "1714087546717";
+let publicKey = "393649b76ca1c0fe1aacb077b44d6351";
+let hashVal = "7c052ac144dc6671c08debec74c7c568";
